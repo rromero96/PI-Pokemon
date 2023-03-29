120cb4b2b953fa44c068a83384e0da12e4235541
Rodrigo Romero <rodrigo.romero@mercadolibre.com>
2023-03-29 12:27:23 -0300

    sql tables

--- cmd/api/pokemon/dto.go
+++ cmd/api/pokemon/dto.go
@@ -1,20 +1,20 @@
 package pokemon
 
 type Pokemon struct {
-	ID      string   `json:"id"`
-	Name    string   `json:"name"`
-	HP      int      `json:"hp"`
-	Attack  int      `json:"attack"`
-	Defense int      `json:"defense"`
-	Image   string   `json:"image"`
-	Speed   int      `json:"speed"`
-	Height  int      `json:"height"`
-	Weight  int      `json:"weight"`
-	Created bool     `json:"created"`
-	Nature  []Nature `json:"types"`
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	HP      int    `json:"hp"`
+	Attack  int    `json:"attack"`
+	Defense int    `json:"defense"`
+	Image   string `json:"image"`
+	Speed   int    `json:"speed"`
+	Height  int    `json:"height"`
+	Weight  int    `json:"weight"`
+	Created bool   `json:"created"`
+	Types   []Type `json:"types"`
 }
 
-type Nature struct {
+type Type struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
