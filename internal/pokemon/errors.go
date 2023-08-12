205b3ce64778798806c8b50100de55059f4f7feb
Rodrigo Romero <rodrigo.romero@mercadolibre.com>
2023-08-11 22:34:54 -0300

    git ignore add

--- internal/pokemon/errors.go
+++ internal/pokemon/errors.go
@@ -6,4 +6,7 @@ var (
 	ErrUnmarshalResponse = errors.New("can't unmarshal response")
 	ErrPokemonNotFound   = errors.New("pokemon not found")
 	ErrTypesNotFound     = errors.New("types not found")
+	ErrCantPerformGet    = errors.New("can't perform get")
+	ErrCantGetPokemon    = errors.New("can't get pokemon")
+	ErrCantGetTypes      = errors.New("can't get types")
 )
